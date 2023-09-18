2c2c6de6dc07aa4bb6032a821fc2588511bcf398
Corfitz <hi@imcorfitz.com>
2023-09-18 15:30:26 +0200

    Fixing a few TW style issues + MD typos

--- components/mdx/mdx.tsx
+++ components/mdx/mdx.tsx
@@ -25,7 +25,7 @@ export function Mdx({ code }: MdxProps) {
   const Component = useMDXComponent(code);
 
   return (
-    <div className="prose prose-img:rounded-lg text-slate-500 dark:text-slate-400 max-w-none prose-p:leading-normal prose-headings:text-slate-800 dark:prose-headings:text-slate-200 prose-a:font-medium prose-a:text-emerald-500 prose-a:no-underline hover:prose-a:underline prose-strong:font-medium prose-strong:text-slate-800 dark:prose-strong:text-slate-100 prose-pre:bg-slate-800 dark:prose-code:text-slate-200">
+    <div className="prose prose-img:rounded-lg text-slate-500 dark:text-slate-400 max-w-none prose-p:leading-normal prose-blockquote:text-inherit prose-headings:text-slate-800 dark:prose-headings:text-slate-200 prose-a:font-medium prose-a:text-emerald-500 prose-a:no-underline hover:prose-a:underline prose-strong:font-medium prose-strong:text-slate-800 dark:prose-strong:text-slate-100 prose-pre:bg-slate-800 dark:prose-code:text-slate-200">
       <Component components={{ ...mdxComponents }} />
     </div>
   );

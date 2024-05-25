9236f40c70f755c26583dcadfc80753b6231aa1a
Corfitz <hi@imcorfitz.com>
2024-05-25 13:47:26 +0200

    Create Footer

---
+++ app/(pages)/_components/footer.tsx
@@ -0,0 +1,32 @@
+export const Footer = () => {
+  return (
+    <div className="flex items-center pb-16 pt-20 font-mono md:pt-40">
+      <p className="text-primary/50 text-xs font-light">
+        <abbr
+          title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License."
+          className="cursor-help"
+        >
+          CC BY-NC 4.0
+        </abbr>{" "}
+        {new Date().getFullYear()} © Corfitz (
+        <a
+          href="https://twitter.com/imCorfitz"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-primary"
+        >
+          @imCorfitz
+        </a>
+        )
+      </p>
+      <a
+        href="https://github.com/imCorfitz/imcorfitz.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="ml-auto border-b border-dotted text-xs"
+      >
+        Source
+      </a>
+    </div>
+  );
+};

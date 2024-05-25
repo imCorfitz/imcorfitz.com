694cb0712e32e619f813ca0130572ce18ff9cea7
Corfitz <hi@imcorfitz.com>
2024-05-25 12:46:35 +0200

    Adding and configuring contentlayer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
+import { withContentlayer } from "next-contentlayer";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withContentlayer(nextConfig);

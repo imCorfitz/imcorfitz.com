b278a8a0165e9d57fa3706285ccef8a797837013
Corfitz <hi@imcorfitz.com>
2025-12-04 10:35:58 +0100

    adding turbopack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import { withContentlayer } from "next-contentlayer2";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  turbopack: {},
+};
 
 export default withContentlayer(nextConfig);
